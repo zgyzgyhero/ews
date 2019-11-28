fda1190824fb6ea9f1cf5f574e9d9f12cc2a6d1a
Muhammad Hewedy <mhewedy@gmail.com>
2019-11-29 01:31:51 +0300

    start working on GetUserAvailability ops

--- ews/get_user_availability.go
+++ ews/get_user_availability.go
@@ -1,3 +1,15 @@
 package ews
 
-//https://docs.microsoft.com/en-us/previous-versions/office/developer/exchange-server-2010/aa564001%28v%3dexchg.140%29
+type GetUserAvailabilityRequest struct {
+	XMLName struct{} `xml:"m:GetUserAvailabilityRequest"`
+}
+
+type GetUserAvailabilityResponse struct {
+}
+
+// GetUserAvailability
+//https://docs.microsoft.com/en-us/exchange/client-developer/web-service-reference/getuseravailability-operation
+func GetUserAvailability(c *Client, r *GetUserAvailabilityRequest) (*GetUserAvailabilityResponse, error) {
+
+	return nil, nil
+}
