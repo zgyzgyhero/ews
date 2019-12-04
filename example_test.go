19ce82379efd0a323900e995ab8cda9029bea921
Muhammad Hewedy <mhewedy@gmail.com>
2019-12-04 20:26:28 +0300

    Polish

--- example_test.go
+++ example_test.go
@@ -2,7 +2,7 @@ package ews_test
 
 import (
 	"fmt"
-	"github.com/mhewedy/ews"
+	. "github.com/mhewedy/ews"
 	"github.com/mhewedy/ews/ewsutil"
 	"log"
 	"time"
@@ -10,11 +10,11 @@ import (
 
 func Example() {
 
-	c := ews.NewClient(
+	c := NewClient(
 		"https://outlook.office365.com/EWS/Exchange.asmx",
 		"example@mhewedy.onmicrosoft.com",
 		"systemsystem@123",
-		&ews.Config{Dump: true},
+		&Config{Dump: true},
 	)
 
 	//err := testSendEmail(c)
@@ -36,7 +36,7 @@ func Example() {
 	fmt.Println("--- success ---")
 }
 
-func testSendEmail(c *ews.Client) error {
+func testSendEmail(c *Client) error {
 	return ewsutil.SendEmail(c,
 		[]string{"mhewedy@gmail.com", "someone@else.com"},
 		"An email subject",
@@ -44,17 +44,17 @@ func testSendEmail(c *ews.Client) error {
 	)
 }
 
-func testCreateCalendarItem(c *ews.Client) error {
-	attendee := make([]ews.Attendee, 0)
+func testCreateCalendarItem(c *Client) error {
+	attendee := make([]Attendee, 0)
 	attendee = append(attendee,
-		ews.Attendee{Mailbox: ews.Mailbox{EmailAddress: "mhewedy@mhewedy.onmicrosoft.com"}},
+		Attendee{Mailbox: Mailbox{EmailAddress: "mhewedy@mhewedy.onmicrosoft.com"}},
 	)
-	attendees := make([]ews.Attendees, 0)
-	attendees = append(attendees, ews.Attendees{Attendee: attendee})
+	attendees := make([]Attendees, 0)
+	attendees = append(attendees, Attendees{Attendee: attendee})
 
-	return ews.CreateCalendarItem(c, ews.CalendarItem{
+	return CreateCalendarItem(c, CalendarItem{
 		Subject: "Planning Meeting",
-		Body: ews.Body{
+		Body: Body{
 			BodyType: "Text",
 			Body:     []byte("Plan the agenda for next week's meeting."),
 		},
@@ -69,41 +69,41 @@ func testCreateCalendarItem(c *ews.Client) error {
 	})
 }
 
-func testGetUserAvailability(c *ews.Client) error {
+func testGetUserAvailability(c *Client) error {
 
-	mb := make([]ews.MailboxData, 0)
-	mb = append(mb, ews.MailboxData{
-		Email: ews.Email{
+	mb := make([]MailboxData, 0)
+	mb = append(mb, MailboxData{
+		Email: Email{
 			Name:        "",
 			Address:     "mhewedy@mhewedy.onmicrosoft.com",
 			RoutingType: "SMTP",
 		},
-		AttendeeType:     ews.AttendeeTypeRequired,
+		AttendeeType:     AttendeeTypeRequired,
 		ExcludeConflicts: false,
-	}, ews.MailboxData{
-		Email: ews.Email{
+	}, MailboxData{
+		Email: Email{
 			Name:        "",
 			Address:     "example2@mhewedy.onmicrosoft.com",
 			RoutingType: "SMTP",
 		},
-		AttendeeType:     ews.AttendeeTypeRoom,
+		AttendeeType:     AttendeeTypeRoom,
 		ExcludeConflicts: false,
 	})
 
 	start, _ := time.Parse(time.RFC3339, "2019-11-28T00:00:00Z")
 	end, _ := time.Parse(time.RFC3339, "2019-12-10T00:00:00Z")
 
-	req := &ews.GetUserAvailabilityRequest{
-		TimeZone: ews.TimeZone{
+	req := &GetUserAvailabilityRequest{
+		TimeZone: TimeZone{
 			Bias: -180,
-			StandardTime: ews.TimeZoneTime{
+			StandardTime: TimeZoneTime{
 				Bias:      0,
 				Time:      "02:00:00",
 				DayOrder:  5,
 				Month:     10,
 				DayOfWeek: "Sunday",
 			},
-			DaylightTime: ews.TimeZoneTime{
+			DaylightTime: TimeZoneTime{
 				Bias:      0,
 				Time:      "02:00:00",
 				DayOrder:  1,
@@ -111,18 +111,18 @@ func testGetUserAvailability(c *ews.Client) error {
 				DayOfWeek: "Sunday",
 			},
 		},
-		MailboxDataArray: ews.MailboxDataArray{MailboxData: mb},
-		FreeBusyViewOptions: ews.FreeBusyViewOptions{
-			TimeWindow: ews.TimeWindow{
+		MailboxDataArray: MailboxDataArray{MailboxData: mb},
+		FreeBusyViewOptions: FreeBusyViewOptions{
+			TimeWindow: TimeWindow{
 				StartTime: start,
 				EndTime:   end,
 			},
 			MergedFreeBusyIntervalInMinutes: 30,
-			RequestedView:                   ews.RequestedViewFreeBusy,
+			RequestedView:                   RequestedViewFreeBusy,
 		},
 	}
 
-	response, err := ews.GetUserAvailability(c, req)
+	response, err := GetUserAvailability(c, req)
 	if err != nil {
 		return err
 	}
@@ -132,16 +132,16 @@ func testGetUserAvailability(c *ews.Client) error {
 	return nil
 }
 
-func testListUsersEvents(c *ews.Client) error {
+func testListUsersEvents(c *Client) error {
 
 	eventUsers := []ewsutil.EventUser{
 		{
 			Email:        "mhewedy@mhewedy.onmicrosoft.com",
-			AttendeeType: ews.AttendeeTypeRequired,
+			AttendeeType: AttendeeTypeRequired,
 		},
 		{
 			Email:        "example2@mhewedy.onmicrosoft.com",
-			AttendeeType: ews.AttendeeTypeRequired,
+			AttendeeType: AttendeeTypeRequired,
 		},
 	}
 	start, _ := time.Parse(time.RFC3339, "2019-11-29T00:00:00Z")
@@ -157,7 +157,7 @@ func testListUsersEvents(c *ews.Client) error {
 	return nil
 }
 
-func testCreateEvent(c *ews.Client) error {
+func testCreateEvent(c *Client) error {
 
 	return ewsutil.CreateEvent(c,
 		[]string{"mhewedy@mhewedy.onmicrosoft.com", "example2@mhewedy.onmicrosoft.com"},
@@ -169,8 +169,8 @@ func testCreateEvent(c *ews.Client) error {
 	)
 }
 
-func testGetRoomLists(c *ews.Client) error {
-	response, err := ews.GetRoomLists(c)
+func testGetRoomLists(c *Client) error {
+	response, err := GetRoomLists(c)
 	if err != nil {
 		return err
 	}
